ac1ce060bb98a0fb29a29d6023de09ca703c0111
abi-stern <69660032+abi-stern@users.noreply.github.com>
2020-08-17 02:51:36 -0400

    Added initial Azure code changes

--- azure.py
+++ azure.py
@@ -1,5 +1,58 @@
-
-
-
 # pow(2, math.ceil(math.log2(127)))
+
+import math 
+from dimension import calculate_dimension_tco
+from azure_instances import managed_disk
+
+cisco_csr = {
+    "t2.medium_infra" : 12, 
+    "t2.medium_software" : 1942, 
+    "c4.2xlarge_infra" : 113, 
+    "c4.2xlarge_software" : 5804,
+    "c4.4xlarge_infra" : 226, 
+    "c4.4xlarge_software" : 7572,
+    }
+
+def calculate_security_cost(number_of_vms, number_of_racks):
+    if number_of_vms <=10:
+        infra = 36 * cisco_csr["t2.medium_infra"] * number_of_racks / 1000 
+        software = 3 * cisco_csr["t2.medium_software"] * number_of_racks / 1000 
+        return infra + software
+    elif number_of_vms <= 50:
+        infra = 36 * cisco_csr["c4.2xlarge_infra"] * number_of_racks / 1000 
+        software = 3 * cisco_csr["c4.2xlarge_software"] * number_of_racks / 1000 
+        return infra + software
+    else:
+        infra = 36 * cisco_csr["c4.4xlarge_infra"] * number_of_racks / 1000 
+        software = 3 * cisco_csr["c4.4xlarge_software"] * number_of_racks / 1000 
+        return infra + software
+
+def calculate_blind_spot_cost(instance, number_of_vms):
+    return ( (instance["rehosting"] + instance["reskilling"])/100 ) * number_of_vms / 1000
+
+
+def calculate_azure_tco_without_blind_spot(instance, dimension, number_of_vms, storage, scenario):
+
+    if scenario == "linux":
+        upfront_cost = instance["linux"] * (1 - instance["discount"]) * number_of_vms * 36 / 1000
+    elif scenario == "windows_with_hybrid_benefit":
+        upfront_cost = instance["windows_with_hybrid_benefit"] * (1 - instance["discount"]) * number_of_vms * 36 / 1000
+    else:
+        upfront_cost = instance["windows"] * (1 - instance["discount"]) * number_of_vms * 36 / 1000
+
+    egress_cost = instance["egress"] * upfront_cost
+
+    managed_disk_cost = number_of_vms * managed_disk[pow(2, math.ceil(math.log2(storage)))] * 36 / 1000
+
+    number_of_racks, dimension_tco = calculate_dimension_tco(dimension, number_of_vms)
+
+    security_cost = calculate_security_cost(number_of_vms, number_of_racks)
+
+    return upfront_cost + egress_cost + ebs_cost + security_cost
+
+
+def calculate_azure_tco_with_blind_spot(instance, dimension, number_of_vms, storage, scenario):
+
+    return calculate_azure_tco_without_blind_spot(instance, dimension, number_of_vms, storage, scenario) + calculate_blind_spot_cost(instance, number_of_vms)
+
 
---
+++ azure_instances.py
@@ -0,0 +1,34 @@
+managed_disk = {
+    4 : 0.6,
+    8 : 1.2,
+    16 : 2.4,
+    32 : 4.81,
+    64 : 9.29, 
+    128 : 17.92,
+    256 : 34.56,
+    512 : 66.56,
+    1024 : 122.88,
+    2048 : 235.52,
+    4096 : 450.56,
+    8192 : 860.16,
+    16384 : 1638.40,
+    32768 : 3276.80
+    }
+
+all_azure_instances_2 = {
+
+    "M5.large" : {
+        "name" : "M5.large",
+        "vCPU" : 2,
+        "memory" : 8,
+        "Linux" : 99.42,
+        "windows_with_hybrid_benefit" : 99.42,
+        "windows" : 99.42,
+        "price_per_month" :99.42,
+        "egress" : 0.05,
+        "discount" : 0.05,
+        "rehosting" : 111000,
+        "reskilling" : 43000
+    },
+
+}
